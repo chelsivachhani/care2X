bc42c8485166ce0d39858693a34bd4d50d86b642
chelsi vachhani <86461537+chelsivachhani@users.noreply.github.com>
2022-09-16 07:26:49 +0530

    table create and model create

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name',255)->nullable();
+            $table->string('last_name',255)->nullable();
+            $table->string('email',255)->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile_number', 255)->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
